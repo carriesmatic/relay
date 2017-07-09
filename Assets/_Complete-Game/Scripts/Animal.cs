a401781392ec943371d86cfce1b64c1ccd7cdb54
Xiaohan Zhang <hellochar@users.noreply.github.com>
2017-07-08 19:10:11 -0700

    Add basic skeleton methods to GameObjects (#1)
    * Add basic skeleton methods to GameObjects
    
    Create dummy scripts representing Animal, Home, and Tile
    
    * Remove extra newline
    
    * Capitalize public property
    
    * Method names should be PascalCase
    
    * Method names should be PascalCase

---
+++ Assets/_Complete-Game/Scripts/Animal.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Relay
+{
+	/// <summary>
+	/// An animal is an Entity that wants to go to a specific home.
+	/// </summary>
+	public class Animal : MonoBehaviour
+	{
+		public Home WantedHome;
+	}
+
+	/// <summary>
+	/// Special type of animal - a bear.
+	/// </summary>
+	public class Bear : Animal
+	{
+	}
+}
+
