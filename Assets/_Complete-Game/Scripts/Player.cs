0b1f0f80436abd86e9b6990c02c7b8d7b13dd2a6
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-08-07 22:17:49 -0700

    delete food and gameover concepts

--- Assets/_Complete-Game/Scripts/Player.cs
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -13,17 +13,12 @@ namespace Relay
 
 		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
 		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
-		public Text foodText;						//UI Text to display current player food total.
 		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
 		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
-		public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
-		public AudioClip eatSound2;					//2 of 2 Audio clips to play when player collects a food object.
-		public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
-		public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
 		public AudioClip gameOverSound;				//Audio clip to play when player dies.
 
 		private Animator animator;					//Used to store a reference to the Player's animator component.
-		private int food;                           //Used to store player food points total during level.
+
 		#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 		private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
 		#endif
@@ -34,9 +29,6 @@ namespace Relay
 		{
 			//Get a component reference to the Player's animator component
 			animator = GetComponent<Animator>();
-
-			//Set the foodText to reflect the current player food total.
-			foodText.text = "Food: 0";
 
 			//Call the Start function of the MovingObject base class.
 			base.Start ();
@@ -126,12 +118,6 @@ namespace Relay
 		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
-			//Every time player moves, subtract from food points total.
-			food--;
-
-			//Update food text display to reflect current score.
-			foodText.text = "Food: " + food;
-
 			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
 			base.AttemptMove <T> (xDir, yDir);
 
@@ -144,9 +130,6 @@ namespace Relay
 				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
 				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
 			}
-
-			//Since the player has moved and lost food points, check if the game has ended.
-			CheckIfGameOver ();
 
 			//Set the playersTurn boolean of GameManager to false now that players turn is over.
 			GameManager.instance.playersTurn = false;
@@ -191,41 +174,6 @@ namespace Relay
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
 		}
 
-
-		//LoseFood is called when an enemy attacks the player.
-		//It takes a parameter loss which specifies how many points to lose.
-		public void LoseFood (int loss)
-		{
-			//Set the trigger for the player animator to transition to the playerHit animation.
-			animator.SetTrigger ("playerHit");
-
-			//Subtract lost food points from the players total.
-			food -= loss;
-
-			//Update the food display with the new total.
-			foodText.text = "-"+ loss + " Food: " + food;
-
-			//Check to see if game has ended.
-			CheckIfGameOver ();
-		}
-
-
-		//CheckIfGameOver checks if the player is out of food points and if so, ends the game.
-		private void CheckIfGameOver ()
-		{
-			//Check if food point total is less than or equal to zero.
-			if (food <= 0) 
-			{
-				//Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
-				SoundManager.instance.PlaySingle (gameOverSound);
-
-				//Stop the background music.
-				SoundManager.instance.musicSource.Stop();
-
-				//Call the GameOver function of GameManager.
-				GameManager.instance.GameOver ();
-			}
-		}
 
 		public bool TryPickUp (Animal a)
 		{
