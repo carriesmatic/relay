62cc1e732be410543de9305a8451885c1318bb72
Carrie Chow <carriercat@gmail.com>
2017-08-19 16:47:58 -0700

    Remove old files and move some stuff around (#12)
    * Remove TutorialInfo
    
    * Remove Mock sprites
    
    * Remove old animations
    
    * Move files around
    
    * Remove TutorialInfo from Main scene
    
    * Remake Home sprite
    
    * Remove unused scene
    
    * Remove vestigal files
    
    * Remove references to _Complete-Game
    
    * Use Level 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,225 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;	//Allows us to use UI.
+using UnityEngine.SceneManagement;
+
+namespace Relay
+{
+	//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+	public class Player : MovingObject
+	{
+		// model state
+		private readonly List<Animal> heldAnimals = new List<Animal>();
+
+		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
+		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
+		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
+		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
+		public AudioClip gameOverSound;				//Audio clip to play when player dies.
+
+		private Animator animator;					//Used to store a reference to the Player's animator component.
+
+		#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+		private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
+		#endif
+
+		//Start overrides the Start function of MovingObject
+		protected override void Start ()
+		{
+			//Get a component reference to the Player's animator component
+			animator = GetComponent<Animator>();
+
+			//Call the Start function of the MovingObject base class.
+			base.Start ();
+		}
+
+		//This function is called when the behaviour becomes disabled or inactive.
+		private void OnDisable ()
+		{
+		}
+
+		private void Update ()
+		{
+			//If it's not the player's turn, exit the function.
+			if (this.isCurrentlyMoving()) return;
+
+			int horizontal = 0;  	//Used to store the horizontal move direction.
+			int vertical = 0;		//Used to store the vertical move direction.
+
+			//Check if we are running either in the Unity editor or in a standalone build.
+			#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+
+			//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+			vertical = (int) (Input.GetAxisRaw ("Vertical"));
+
+			//Check if moving horizontally, if so set vertical to zero.
+			if(horizontal != 0)
+			{
+				vertical = 0;
+			}
+			//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+			#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+			//Check if Input has registered more than zero touches
+			if (Input.touchCount > 0)
+			{
+			//Store the first touch detected.
+			Touch myTouch = Input.touches[0];
+
+			//Check if the phase of that touch equals Began
+			if (myTouch.phase == TouchPhase.Began)
+			{
+			//If so, set touchOrigin to the position of that touch
+			touchOrigin = myTouch.position;
+			}
+
+			//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+			else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+			{
+			//Set touchEnd to equal the position of this touch
+			Vector2 touchEnd = myTouch.position;
+
+			//Calculate the difference between the beginning and end of the touch on the x axis.
+			float x = touchEnd.x - touchOrigin.x;
+
+			//Calculate the difference between the beginning and end of the touch on the y axis.
+			float y = touchEnd.y - touchOrigin.y;
+
+			//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+			touchOrigin.x = -1;
+
+			//Check if the difference along the x axis is greater than the difference along the y axis.
+			if (Mathf.Abs(x) > Mathf.Abs(y))
+			//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+			horizontal = x > 0 ? 1 : -1;
+			else
+			//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+			vertical = y > 0 ? 1 : -1;
+			}
+			}
+
+			#endif //End of mobile platform dependendent compilation section started above with #elif
+			//Check if we have a non-zero value for horizontal or vertical
+			if(horizontal != 0 || vertical != 0)
+			{
+				AttemptMove<MonoBehaviour>(horizontal, vertical);
+			}
+		}
+
+		//AttemptMove overrides the AttemptMove function in the base class MovingObject
+		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+		protected override void AttemptMove <T> (int xDir, int yDir)
+		{
+			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+			base.AttemptMove <T> (xDir, yDir);
+
+			//Hit allows us to reference the result of the Linecast done in Move.
+			RaycastHit2D hit;
+
+			//If Move returns true, meaning Player was able to move into an empty space.
+			if (Move (xDir, yDir, out hit)) 
+			{
+				GameManager.instance.EndTurn();
+
+				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+			}
+		}
+
+
+		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
+		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+		protected override void OnCantMove <T> (T component)
+		{
+		}
+
+		protected override void AnimateDirection(Direction d)
+		{
+			switch (d)
+			{
+			case Direction.Up:
+				animator.SetTrigger("walkBack");
+				break;
+			case Direction.Down:
+				animator.SetTrigger("walkFront");
+				break;
+			case Direction.Left:
+				animator.SetTrigger("walkLeft");
+				break;
+			case Direction.Right:
+				animator.SetTrigger("walkRight");
+				break;
+			}
+		}
+
+		//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			//Check if the tag of the trigger collided with is Exit.
+			if(other.tag == "Exit")
+			{
+				//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+				Invoke ("Restart", restartLevelDelay);
+
+				//Disable the player object since level is over.
+				enabled = false;
+			}
+		}
+
+		private void Restart ()
+		{
+			//Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
+			//and not load all the scene object in the current scene.
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+		}
+
+		public bool TryPickUp (Animal a)
+		{
+			float distanceToAnimal = Mathf.Abs (this.transform.position.x - a.transform.position.x) + Mathf.Abs (this.transform.position.y - a.transform.position.y);
+			if (this.heldAnimals.Count > 2)
+			{
+				// hands are full
+				return false;
+			}
+			else if (distanceToAnimal > 1)
+			{
+				// animal is too far away to grab
+				return false;
+			}
+			else if (/* TODO animal is not in the map */ false)
+			{
+				return false;
+			}
+			else
+			{
+				this.heldAnimals.Add (a);
+				return true;
+			}
+		}
+
+		/**
+		 * Is "dropping" an animal off the same as putting it into a home?
+		 * 
+		 * Do animals automatically enter their home, or do you need to drop it?
+		 * 
+		 * returns true if successfully dropped, false if not
+		 */
+		public bool TryDrop (Animal a, int dropX, int dropY)
+		{
+			if (heldAnimals.Contains (a))
+			{
+				heldAnimals.Remove (a);
+				// TODO somehow add the animal back onto the Level at dropX dropY
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
