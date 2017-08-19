c0a6c284fcb5377f64b465da727a8bfdfb056696
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-08-19 00:39:50 -0700

    wip; bear pickup; no feedback/ui

--- Assets/_Complete-Game/Scripts/Player.cs
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -33,11 +33,6 @@ namespace Relay
 			base.Start ();
 		}
 
-		//This function is called when the behaviour becomes disabled or inactive.
-		private void OnDisable ()
-		{
-		}
-
 		private void Update ()
 		{
 			//If it's not the player's turn, exit the function.
@@ -107,43 +102,26 @@ namespace Relay
 			{
 				//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
 				//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
-				AttemptMove<Wall> (horizontal, vertical);
+				AttemptMove (horizontal, vertical);
 			}
 		}
 
-		//AttemptMove overrides the AttemptMove function in the base class MovingObject
-		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
-		protected override void AttemptMove <T> (int xDir, int yDir)
+		protected override void OnMoveBegan()
 		{
-			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
-			base.AttemptMove <T> (xDir, yDir);
+			GameManager.instance.EndTurn();
 
-			//Hit allows us to reference the result of the Linecast done in Move.
-			RaycastHit2D hit;
-
-			//If Move returns true, meaning Player was able to move into an empty space.
-			if (Move (xDir, yDir, out hit)) 
+			//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+			SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+		}
+			
+		protected override void OnMoveBlocked (Transform component)
+		{
+			Animal animal = component.GetComponent<Animal> ();
+			if (animal != null)
 			{
-				GameManager.instance.EndTurn();
-
-				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
-				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+				// this is an animal; pick it up if we can
+				TryPickUp(animal);
 			}
-		}
-
-
-		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
-		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
-		protected override void OnCantMove <T> (T component)
-		{
-			//Set hitWall to equal the component passed in as a parameter.
-			Wall hitWall = component as Wall;
-
-			//Call the DamageWall function of the Wall we are hitting.
-			hitWall.DamageWall (wallDamage);
-
-			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
-			animator.SetTrigger ("playerChop");
 		}
 
 
@@ -188,6 +166,7 @@ namespace Relay
 			else
 			{
 				this.heldAnimals.Add (a);
+				a.transform.gameObject.SetActive (false);
 				return true;
 			}
 		}
