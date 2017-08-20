5ec53343a9f5846a7b0752babb2600c77f1bf392
Carrie Chow <carriercat@gmail.com>
2017-08-19 18:47:11 -0700

    Can enter game through menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Relay
+{
+	public class MainMenu : MonoBehaviour
+	{
+		public void StartGame(string startScene)
+		{
+			SceneManager.LoadScene(startScene, LoadSceneMode.Single);
+		}
+	}
+}
