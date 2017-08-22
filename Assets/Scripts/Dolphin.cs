17b5add15093a6cc75809ba697c136cf93ef865a
Carrie Chow <carriercat@gmail.com>
2017-08-21 21:39:26 -0700

    Make animal scripts for rabbit and dolphin

---
+++ Assets/Scripts/Dolphin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Relay
+{
+	/// <summary>
+	/// Special type of animal - a dolphin.
+	/// </summary>
+	public class Dolphin : Animal
+	{
+	}
+}
+
