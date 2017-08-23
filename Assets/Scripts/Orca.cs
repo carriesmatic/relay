d062ff90e1c2682dd4fc8eeba68d708f502ff8a1
Carrie Chow <carriercat@gmail.com>
2017-08-23 16:01:01 -0700

    Update animal sprites

---
+++ Assets/Scripts/Orca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Relay
+{
+	/// <summary>
+	/// Special type of animal - an orca.
+	/// </summary>
+	public class Orca : Animal
+	{
+	}
+}
+
